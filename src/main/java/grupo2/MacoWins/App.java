5406730e53ce847835ed07362308ca80cdd61b04
Leila Weicman <leilaweic@hotmail.com>
2016-03-24 18:52:16 -0300

    creacion de repositorio

---
+++ src/main/java/grupo2/MacoWins/App.java
@@ -0,0 +1,13 @@
+package grupo2.MacoWins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
