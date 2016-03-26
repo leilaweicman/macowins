ebbcb4a221f33070f08ac4f2f12e0b13d31038a2
FBerco <berco.fede@gmail.com>
2016-03-26 13:14:41 -0300

    Agrego propiedades a la prenda y me devuelve PrecioFinal

--- src/main/java/grupo2/MacoWins/Classes/Prenda.java
+++ src/main/java/grupo2/MacoWins/Classes/Prenda.java
@@ -1,5 +1,14 @@
 package grupo2.MacoWins.Classes;
 
 public class Prenda {
-
+	public int Precio;
+	public boolean Importado;
+	
+	public double PrecioFinal (int valorNegocio){
+		double precioFinal = valorNegocio * Precio;
+		if (Importado) {
+			return precioFinal * (1.3);
+		}
+		return precioFinal;
+	}
 }
