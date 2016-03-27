b62cc75fe5e7321d7c20d53d8409623f1e7286f2
FBerco <berco.fede@gmail.com>
2016-03-27 20:09:45 -0300

    Refactor

--- src/main/java/grupo2/MacoWins/App.java
+++ src/main/java/grupo2/MacoWins/App.java
@@ -11,6 +11,10 @@ public class App
     {
     	listPrendas = new ArrayList<Prenda>();
     	listVentas = new ArrayList<Venta>();
+    	RegistrarVentas();
+    }
+    
+    public static void RegistrarVentas(){
     	Scanner in = new Scanner(System.in);    	
     	String prenda;
     	int cantidad;
@@ -20,35 +24,40 @@ public class App
     	while(!prenda.equals("terminar")){
         	System.out.println("Ingresar cantidad");
         	cantidad = in.nextInt();
-        	Venta v = new Venta();
-        	v.cantidad = cantidad;
         	
-    		//Lo implemento con ifs porque no me deja hacer un switch de strings
-    		//Hay que ver como manejar el tema de si es importado o no.
-    		//Por ahora seteo todo en importado
-    		if(prenda.equals("c")){
-    			Camisa c = new Camisa(true);
-    			listPrendas.add(c);
-    			cantCamisas += cantidad;
-    			v.prenda = c;
-    		}else if (prenda.equals("p")){
-    			Pantalon p = new Pantalon(true);
-    			listPrendas.add(p);
-    			cantPantalones+= cantidad;  
-    			v.prenda = p;
-    		}else if(prenda.equals("s")){
-    			Saco s = new Saco(true);
-    			listPrendas.add(s);
-    			cantSacos+= cantidad;
-    			v.prenda = s;
-    		}			
-			listVentas.add(v);
-			
-        	System.out.println("Prenda agregada");
+        	RegistrarVenta(prenda, cantidad);
+        	
         	System.out.println("Ingresar prenda (c: camisa, p: pantalon, s:saco");
         	prenda = in.next();
     	}
     	System.out.println("terminó");    	
     	
+    }
+    
+    public static void RegistrarVenta(String prenda, int cantidad){
+    	Venta v = new Venta();
+    	v.cantidad = cantidad;
+    	
+		//Lo implemento con ifs porque no me deja hacer un switch de strings
+		//Hay que ver como manejar el tema de si es importado o no.
+		//Por ahora seteo todo en importado
+		if(prenda.equals("c")){
+			Camisa c = new Camisa(true);
+			listPrendas.add(c);
+			cantCamisas += cantidad;
+			v.prenda = c;
+		}else if (prenda.equals("p")){
+			Pantalon p = new Pantalon(true);
+			listPrendas.add(p);
+			cantPantalones+= cantidad;  
+			v.prenda = p;
+		}else if(prenda.equals("s")){
+			Saco s = new Saco(true);
+			listPrendas.add(s);
+			cantSacos+= cantidad;
+			v.prenda = s;
+		}			
+		listVentas.add(v);		
+    	System.out.println("Prenda agregada");
     }
 }
